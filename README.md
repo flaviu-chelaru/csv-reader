f824e9147693fd725311eaccebdc1b2e171f046d
Flaviu Chelaru <flaviu.chelaru@gmail.com>
2018-07-21 10:03:32 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # csv-reader
 A CSV reader based on iterators. Offers better support for line endings
+
+## Features
+- custom line endings
+- ability to pause / resume reading
+
+## usage
+
+```php
+
+use cfv1000\CsvReader\Reader;
+
+$csv = new Reader($filename);
+// $csv->seek(); jump to specified pointer. Suggestion: only use values returned by $csv->tell(). Will cause weird results otherwise. (if reading from the middle of the line, for example) 
+foreach($csv as $csvLine)
+{
+ // print $csv->tell(); -> returns current position in the file
+ print_r($csvLine);
+} 
+```
---
+++ src/Reader.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace cfv1000\CsvReader;
+
+use Iterator;
+use SplFileObject;
+use SplFileInfo;
+
+class Reader implements Iterator
+{
+    /**
+     * @var SplFileObject
+     */
+    private $file;
+
+    /**
+     * @var int
+     */
+    private $key = 0;
+
+    /**
+     * @var string
+     */
+    private $columnSeparator;
+
+    /**
+     * @var string
+     */
+    private $lineSeparator;
+
+    /**
+     * Column enclosure character
+     *
+     * @var string
+     */
+    private $enclosure;
+
+    /**
+     * Reader constructor.
+     *
+     * @param string $file
+     * @param string $columnSeparator
+     * @param string $enclosure
+     * @param string $lineSeparator
+     */
+    public function __construct(string $file, $columnSeparator = ',', $enclosure = '', $lineSeparator = PHP_EOL)
+    {
+        $fileInfo = new SplFileInfo($file);
+        $this->file = $fileInfo->openFile('rb+');
+
+        $this->columnSeparator = $columnSeparator;
+        $this->lineSeparator = $lineSeparator;
+        $this->enclosure = $enclosure;
+
+        $this->next();
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return !$this->file->eof();
+    }
+
+    public function next()
+    {
+        ++$this->key;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function current(): array
+    {
+        $buffer = '';
+        do {
+            $buffer .= $this->file->fread(1);
+            $matchLineSeparator = strpos($buffer, $this->lineSeparator);
+        } while ($this->valid() && !$matchLineSeparator);
+
+        $line = explode($this->columnSeparator, $buffer);
+
+        return array_map(function ($item) {
+            return trim($item, " \t\n\r\0\x0B" . $this->enclosure);
+        }, $line);
+    }
+
+    /**
+     * @return int
+     */
+    public function key(): int
+    {
+        return $this->key;
+    }
+
+    public function rewind()
+    {
+        $this->key = 0;
+    }
+
+    /**
+     * Jump to $offset file pointer
+     *
+     * @param int $offset
+     */
+    public function seek(int $offset)
+    {
+        $this->file->fseek($offset);
+    }
+
+    /**
+     * Return current file pointer
+     *
+     * @return int
+     */
+    public function tell(): int
+    {
+        return $this->file->ftell();
+    }
+}
